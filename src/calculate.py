48284dab15f40f50d04b0416f827f6ec5a9046a8
Mark Ethan <mimicethan@gmail.com>
2026-05-22 11:36:26 +0800

    api

--- src/calculate.py
+++ src/calculate.py
@@ -1,3 +1,7 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
 def add_f(a, b):
     return a + b
 
@@ -8,3 +12,15 @@ def sub_f(a, b):
 
 def mut_f(a: float, b: float) -> float:
     return a * b
+
+
+@app.get("/")
+def home():
+    return {"status": "Online", "message": "這是簡易計算機 API"}
+
+
+@app.get("/add")
+def calculate_add(a: float, b: float):
+    # add_func
+    result = add_f(a, b)
+    return {"operation": "addition", "x": a, "b": b, "result": result}
