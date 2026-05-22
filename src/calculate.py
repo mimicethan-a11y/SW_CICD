a5795c46999e8cbd3a0234aab3d60ba91ffcbb36
Mark Ethan <mimicethan@gmail.com>
2026-05-22 10:50:17 +0800

    reformat src

--- src/calculate.py
+++ src/calculate.py
@@ -1,3 +1,2 @@
-def add_f(a,b):
-    return a+b
-
+def add_f(a, b):
+    return a + b
--- src/test_calculate.py
+++ src/test_calculate.py
@@ -1,6 +1,7 @@
 from calculate import add_f
 
+
 def add_test_f():
-    assert add_f(1,2) == 3
-    assert add_f(0,0) == 0
-    assert add_f(-1,-2) == -3
+    assert add_f(1, 2) == 3
+    assert add_f(0, 0) == 0
+    assert add_f(-1, -2) == -3
